c263431be1282b88f0ca6c6f472bfc8a2c6f5dbf
Amanda Koh <amanda@naucera.net>
2018-05-09 07:40:37 +1000

    Support selectively bumping major/minor/revision.

--- main.go
+++ main.go
@@ -6,15 +6,26 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
 )
+
+func bumpVersion(versionPart string) string {
+	ver, err := strconv.ParseUint(versionPart, 10, 64)
+	if err != nil {
+		return "0"
+	}
+
+	return strconv.FormatUint(ver + 1, 10)
+}
 
 func main() {
 
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: go-version <package_name>\n")
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "Usage: go-version <bump> <package_name>\n")
 		os.Exit(1)
 	}
-	packageName := os.Args[1]
+	bump := os.Args[1]
+	packageName := os.Args[2]
 
 	tagPattern := regexp.MustCompile(`^\s*([^.]+)\.([^.]+)\.(.+)\s*$`)
 
@@ -31,6 +42,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	verMajor, verMinor, verRev := matches[1], matches[2], matches[3]
+
+	switch bump {
+	case "major": verMajor, verMinor, verRev = bumpVersion(verMajor), "0", "0"
+	case "minor": verMinor, verRev = bumpVersion(verMinor), "0"
+	default: case "revision": verRev = bumpVersion(verRev)
+	}
+
 	verFile, err := os.Create("version.go")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
@@ -43,11 +62,11 @@ func main() {
 	writer.WriteString("\n")
 	writer.WriteString(fmt.Sprintf("//go:generate go run vendor/github.com/mandykoh/go-version/main.go %s\n", packageName))
 	writer.WriteString("\n")
-	writer.WriteString(fmt.Sprintf("const VersionMajor = \"%s\"\n", matches[1]))
-	writer.WriteString(fmt.Sprintf("const VersionMinor = \"%s\"\n", matches[2]))
-	writer.WriteString(fmt.Sprintf("const VersionRevision = \"%s\"\n", matches[3]))
-	writer.WriteString(fmt.Sprintf("const Version = \"%s.%s.%s\"\n", matches[1], matches[2], matches[3]))
+	writer.WriteString(fmt.Sprintf("const VersionMajor = \"%s\"\n", verMajor))
+	writer.WriteString(fmt.Sprintf("const VersionMinor = \"%s\"\n", verMinor))
+	writer.WriteString(fmt.Sprintf("const VersionRevision = \"%s\"\n", verRev))
+	writer.WriteString(fmt.Sprintf("const Version = \"%s.%s.%s\"\n", verMajor, verMinor, verRev))
 	writer.Flush()
 
-	fmt.Printf("Generated version %s.%s.%s for package %s\n", matches[1], matches[2], matches[3], packageName)
+	fmt.Printf("Generated version %s.%s.%s for package %s\n", verMajor, verMinor, verRev, packageName)
 }
